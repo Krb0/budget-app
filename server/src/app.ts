7aeb87813fed51c02c5fcb93b9c0f120a9398002
krb0 <maurodavidcaceres@gmail.com>
2022-06-05 18:54:50 -0300

    users route added

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,10 @@
 import bodyParser from 'body-parser';
 import express from 'express';
+import UsersRoute from './users/users.routes';
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use('/users', UsersRoute);
 export default app;
