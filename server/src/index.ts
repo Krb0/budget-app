326c08dd4f6a7238e36ba0df664b63c23dcf1467
krb0 <maurodavidcaceres@gmail.com>
2022-06-05 18:55:16 -0300

    app running

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,6 @@
 import databaseConfig from '../config/database.config';
 import app from './app';
 
-databaseConfig.initialize().then(() => console.log('opened'));
+databaseConfig.initialize().then(() => {
+  app.listen(5000);
+});
