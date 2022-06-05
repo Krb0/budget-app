115cdc2b5b33022c0bd6d41acd963d0fa188d134
krb0 <maurodavidcaceres@gmail.com>
2022-06-05 18:55:02 -0300

    user entity added

--- server/config/database.config.ts
+++ server/config/database.config.ts
@@ -1,14 +1,17 @@
 import { DataSource } from 'typeorm';
 import dotenv from 'dotenv';
+import { User } from '../src/entities/user.entity';
 dotenv.config();
-export default new DataSource({
+const database = new DataSource({
   type: 'postgres',
   host: process.env.DB_HOST,
   port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  entities: [],
+  entities: [User],
   synchronize: true,
   logging: false,
 });
+
+export default database;
