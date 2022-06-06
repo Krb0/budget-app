c84e3011b6c5b17d51ac9a78e1ace1cecaace1c4
krb0 <maurodavidcaceres@gmail.com>
2022-06-06 10:34:17 -0300

    login added to service

--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,13 +1,35 @@
+import { hash, verify } from 'argon2';
 import database from '../../config/database.config';
 import { User } from '../entities/user.entity';
 
 export class UserService {
   async register({ email, password }: Partial<User>) {
+    try {
+      if (email && password) {
+        if (email.length > 7 && password.length >= 8) {
+          const repo = database.getRepository(User);
+          const hashedPass = await hash(password);
+          const user = repo.create({ email, password: hashedPass });
+          await repo.save(user);
+          return user;
+        }
+      }
+    } catch (err) {
+      return null;
+    }
+  }
+  async login({ email, password }: User) {
     if (email && password) {
       const repo = database.getRepository(User);
-      const user = repo.create({ email, password });
-      await repo.save(user);
-      return user;
+      const user = await repo.findOne({
+        where: {
+          email: email,
+        },
+      });
+      if (user) {
+        const verification = await verify(user.password, password);
+        if (verification) return user;
+      }
     }
     return null;
   }
