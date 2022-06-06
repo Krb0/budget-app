3fe27b1c9024f5274d26f2a6aa2f9df89489aaaa
krb0 <maurodavidcaceres@gmail.com>
2022-06-06 10:32:43 -0300

    cors middleware added

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,12 @@
 import bodyParser from 'body-parser';
 import express from 'express';
 import UsersRoute from './users/users.routes';
+import cors from 'cors';
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
+app.use(cors());
 app.use('/users', UsersRoute);
 export default app;
+    
