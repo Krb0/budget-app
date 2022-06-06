7ed9f1e44fe857d3cebd71c6ff98425614a5deb2
krb0 <maurodavidcaceres@gmail.com>
2022-06-06 10:33:48 -0300

    login to controller added

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -5,11 +5,22 @@ import { UserService } from './users.service';
 export class UserController {
   constructor(private user: UserService) {}
 
-  async register(req: Request, res: Response, next: NextFunction): Promise<void> {
+  async register(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
     const body = req.body;
     const user = await this.user.register(body);
     user
       ? res.status(200).json(user)
       : res.status(400).json({ message: 'User not created' });
+  }
+  async login(req: Request, res: Response, next: NextFunction): Promise<void> {
+    const body = req.body;
+    const user = await this.user.login(body);
+    user
+      ? res.status(200).json(user)
+      : res.status(400).json({ message: 'Invalid credentials' });
   }
 }
